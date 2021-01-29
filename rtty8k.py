73f907917c6a902dfc12055bab8f787349381a9c
michihiro imaoka <imaoca@gmail.com>
2021-01-30 08:44:10 +0900

    Update rtty8k.py
    change window process

--- rtty8k.py
+++ rtty8k.py
@@ -2,10 +2,10 @@ import wave
 import numpy as np
 
 fname='rtty3s.wav' # should be specify the filename.
-Smp= 8000          # Sampling Rate
-FQm= Smp/914.0     # Mark Frequency 914Hz
-FQs= Smp/1086.0    # Space Frequency 1086Hz
-Baud= 176          # Smp/45.45 but shuold be Integer
+smp= 8000          # Sampling Rate
+FQm= smp/914.0     # Mark Frequency 914Hz
+FQs= smp/1086.0    # Space Frequency 1086Hz
+wind= 176          # wind = smp/45.45 but shuold be Integer
 
 waveFile = wave.open(fname, 'r')
 buf = waveFile.readframes(-1)
@@ -18,6 +18,7 @@ for i in range(0, 8000):
      Is.append((buf[i]-128)*np.sin(np.pi*2.0/FQs*i))
      Qs.append((buf[i]-128)*np.cos(np.pi*2.0/FQs*i))
 
-for i in range(0, 8000-Baud):
-     print (np.sqrt(sum(Im[i:i+Baud])**2 + sum(Qm[i:i+Baud])**2),end=",")
-     print (np.sqrt(sum(Is[i:i+Baud])**2 + sum(Qs[i:i+Baud])**2))
+     if (i>wind):
+          mk = np.sqrt(sum(Im[i-wind:i])**2 + sum(Qm[i-wind:i])**2)
+          sp = np.sqrt(sum(Is[i-wind:i])**2 + sum(Qs[i-wind:i])**2)
+          print (mk,end=",");print (sp)
