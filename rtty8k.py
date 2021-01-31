441a6aa76233e8e20ff3bdc6dd59033b026b9c5d
michihiro imaoka <imaoca@gmail.com>
2021-01-31 09:32:18 +0900

    Update rtty8k.py
    Improve to fewer computing resources

--- rtty8k.py
+++ rtty8k.py
@@ -1,24 +1,22 @@
 import wave
 import numpy as np
-
 fname='rtty3s.wav' # should be specify the filename.
 smp= 8000          # Sampling Rate
 FQm= smp/914.0     # Mark Frequency 914Hz
 FQs= smp/1086.0    # Space Frequency 1086Hz
-wind= 176          # wind = smp/45.45 but shuold be Integer
-
+wind= 32           # windows size Integer
 waveFile = wave.open(fname, 'r')
-buf = waveFile.readframes(-1)
+j=0
+for i in range(int(waveFile.getnframes()/wind)):
+     buf = waveFile.readframes(wind)
+     Im=0;Qm=0;Is=0;Qs=0
+     for c in buf:
+          Im=Im+((c-128)*np.sin(np.pi*2.0/FQm*j))
+          Qm=Qm+((c-128)*np.cos(np.pi*2.0/FQm*j))
+          Is=Is+((c-128)*np.sin(np.pi*2.0/FQs*j))
+          Qs=Qs+((c-128)*np.cos(np.pi*2.0/FQs*j))
+          j = j + 1
+     mk = np.sqrt(Im**2 + Qm**2)
+     sp = np.sqrt(Is**2 + Qs**2)     
+     print(mk,sp,int(mk>sp),sep=",")
 waveFile.close()
-
-Im=[];Qm=[];Is=[];Qs=[]
-for i in range(0, 8000):
-     Im.append((buf[i]-128)*np.sin(np.pi*2.0/FQm*i))
-     Qm.append((buf[i]-128)*np.cos(np.pi*2.0/FQm*i))
-     Is.append((buf[i]-128)*np.sin(np.pi*2.0/FQs*i))
-     Qs.append((buf[i]-128)*np.cos(np.pi*2.0/FQs*i))
-
-     if (i>wind):
-          mk = np.sqrt(sum(Im[i-wind:i])**2 + sum(Qm[i-wind:i])**2)
-          sp = np.sqrt(sum(Is[i-wind:i])**2 + sum(Qs[i-wind:i])**2)
-          print (mk,end=",");print (sp)
