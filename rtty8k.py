11d1164a29263da84043dd15b3698e809e22c234
michihiro imaoka <imaoca@gmail.com>
2021-02-04 14:54:32 +0900

    Update rtty8k.py
    use list data structure as FIFO.

--- rtty8k.py
+++ rtty8k.py
@@ -6,17 +6,17 @@ FQm= smp/914.0     # Mark Frequency 914Hz
 FQs= smp/1086.0    # Space Frequency 1086Hz
 wind= 32           # windows size Integer
 waveFile = wave.open(fname, 'r')
-j=0
-for i in range(int(waveFile.getnframes()/wind)):
-     buf = waveFile.readframes(wind)
-     Im=0;Qm=0;Is=0;Qs=0
-     for c in buf:
-          Im=Im+((c-128)*np.sin(np.pi*2.0/FQm*j))
-          Qm=Qm+((c-128)*np.cos(np.pi*2.0/FQm*j))
-          Is=Is+((c-128)*np.sin(np.pi*2.0/FQs*j))
-          Qs=Qs+((c-128)*np.cos(np.pi*2.0/FQs*j))
-          j = j + 1
-     mk = np.sqrt(Im**2 + Qm**2)
-     sp = np.sqrt(Is**2 + Qs**2)     
-     print(mk,sp,int(mk>sp),sep=",")
+
+mq=[];mi=[];sq=[];si=[]
+for j in range(waveFile.getnframes()):
+      buf = waveFile.readframes(1)
+      mq.append((buf[0]-128)*np.sin(np.pi*2.0/FQm*j))
+      mi.append((buf[0]-128)*np.cos(np.pi*2.0/FQm*j))
+      sq.append((buf[0]-128)*np.sin(np.pi*2.0/FQs*j))
+      si.append((buf[0]-128)*np.cos(np.pi*2.0/FQs*j))
+      mk = np.sqrt(sum(mq)**2 + sum(mi)**2)
+      sp = np.sqrt(sum(sq)**2 + sum(si)**2)     
+      print(mk,sp,int(mk>sp),sep=",")
+      if j>wind:
+            mq.pop(0);mi.pop(0);sq.pop(0);si.pop(0)
 waveFile.close()
